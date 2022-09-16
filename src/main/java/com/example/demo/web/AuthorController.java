aeaeea183417bcf763f728be5b6556ddcfbf1ffc
Luc <luc.schulze@ecube.de>
2022-09-16 16:31:55 +0200

    refactored CRUD operation methods

--- src/main/java/com/example/demo/web/AuthorController.java
+++ src/main/java/com/example/demo/web/AuthorController.java
@@ -1,60 +1,37 @@
 package com.example.demo.web;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import java.util.*;
 
 @RestController
 @ResponseBody
 @RequestMapping(value = {"/api/author"})
 class AuthorController {
 
-    private Map<String, Author> authorMap = new HashMap<>();
-
-    public String errorMessage = "The author you requested doesn't exist. Please review your parameters!";
-
-    private Object errorChecking(String authorId) {
-        if(authorMap.get(authorId) == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, this.errorMessage);
-        }
-        return new ResponseStatusException(HttpStatus.NOT_FOUND, this.errorMessage);
-    }
-
     @PostMapping(consumes = {"application/json"},
             produces = {"application/json"})
-    Author addAuthor(@RequestBody Author author) {
-        String authorId = UUID.randomUUID().toString();
-        author.setId(authorId);
-        authorMap.put(authorId, author);
-        return author;
+    Authors addAuthor(@RequestBody Authors author) {
+        return DatenBank.postAuthors(author);
     }
 
     @ResponseBody
     @GetMapping(value = {"/{authorId}"},
             produces = {"application/json"})
-    Author author(@PathVariable String authorId){
-        errorChecking(authorId);
-        Author getAuthor = authorMap.get(authorId);
-        return getAuthor;
+    Authors getAuthor(@PathVariable String authorId){
+        return DatenBank.getAuthors(authorId);
     }
 
     @ResponseBody
     @DeleteMapping(value = {"/{authorId}"},
             consumes = {"application/json"})
     void removeAuthor(@PathVariable String authorId){
-        errorChecking(authorId);
-        authorMap.remove(authorId);
+        DatenBank.deleteAuthors(authorId);
     }
 
     @ResponseBody
     @PutMapping(value = {"/{authorId}"},
             consumes = {"application/json"},
             produces = {"application/json"})
-    Author updateAuthor(@PathVariable String authorId, @RequestBody Author authorFromUser){
-        errorChecking(authorId);
-        authorFromUser.setId(authorId);
-        authorMap.replace(authorId, authorFromUser);
-        return authorFromUser;
+    Authors updateAuthor(@PathVariable String authorId, @RequestBody Authors authorFromUser){
+        return DatenBank.putAuthors(authorId, authorFromUser);
     }
 }
